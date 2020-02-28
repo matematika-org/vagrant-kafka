c2c9ffb59472224ec1975f21c32b21625c1a00c3
Marley <jobpoisk@gmail.com>
2020-02-28 23:38:52 +0300

    upd

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # vagrant-kafka
-vagrant-kafka
+
+Разворачиваем kafka сервер vagrant скриптами.
+
+Подробнее <a href="https://matematika.org/bigdata/kafka/install/linux/">здесь</a>
---
+++ centos7/bootstrap.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+yum update && yum upgrade -y
+
+# Update hosts file
+echo "[TASK 1] Update /etc/hosts file"
+cat >>/etc/hosts<<EOF
+192.168.0.11 kafka1
+192.168.0.11 zookeeper1
+192.168.0.12 kafka2
+192.168.0.12 zookeeper2
+192.168.0.13 kafka3
+192.168.0.13 zookeeper3
+EOF
+
+# Disable SELinux
+echo "[TASK 4] Disable SELinux"
+setenforce 0
+sed -i --follow-symlinks 's/^SELINUX=enforcing/SELINUX=disabled/' /etc/sysconfig/selinux
+
+# Disable swap
+echo "[TASK 7] Disable and turn off SWAP"
+sed -i '/swap/d' /etc/fstab
+swapoff -a
+
+
+# Add vagrant user to docker group
+#usermod -aG user root > /dev/null 2>&1
