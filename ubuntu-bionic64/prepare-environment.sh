aa23f442f4496ddb307158aa9862b68e6f8e30f7
Marley <jobpoisk@gmail.com>
2020-02-28 21:30:27 +0300

    upd

---
+++ ubuntu-bionic64/prepare-environment.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Kubelet не будет работать, если включен swap
+# Отключаю Своп
+swapoff -a && sed -i '/ swap / s/^/#/' /etc/fstab
+
+apt-get update && apt-get upgrade -y && apt-get install -y default-jdk
+
+
+# Прописываю хосты
+echo "192.168.0.11 kafka1" >> /etc/hosts
+echo "192.168.0.11 zookeeper1" >> /etc/hosts
+echo "192.168.0.12 kafka2" >> /etc/hosts
+echo "192.168.0.12 zookeeper2" >> /etc/hosts
+echo "192.168.0.13 kafka3" >> /etc/hosts
+echo "192.168.0.13 zookeeper3" >> /etc/hosts
